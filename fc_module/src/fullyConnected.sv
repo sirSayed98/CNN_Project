ddea97bebc9977eb7f9a3f913e76245f65d310e3
mohsayed27 <mohsayed27@gmail.com>
2021-05-23 15:13:32 +0200

    FC: added OP_LAYER_SIZE param

--- fc_module/src/fullyConnected.sv
+++ fc_module/src/fullyConnected.sv
@@ -1,28 +1,29 @@
 module fullyConnected#(
     parameter WORD_SIZE = 16,
-    parameter LAYER_SIZE = 128,
+    parameter IP_LAYER_SIZE = 128,
+    parameter OP_LAYER_SIZE = 84,
     parameter INT_SLICE = 8
     )
     (
-    output [WORD_SIZE-1 : 0] Z [LAYER_SIZE-1 : 0],
-	input  [WORD_SIZE-1 : 0] X [LAYER_SIZE-1 : 0],
-    input  [WORD_SIZE-1 : 0] W [LAYER_SIZE-1 : 0][LAYER_SIZE-1 : 0],
-    input  [WORD_SIZE-1 : 0] B [LAYER_SIZE-1 : 0]
+    output [WORD_SIZE-1 : 0] Z [OP_LAYER_SIZE-1 : 0],
+	input  [WORD_SIZE-1 : 0] X [IP_LAYER_SIZE-1 : 0],
+    input  [WORD_SIZE-1 : 0] W [OP_LAYER_SIZE-1 : 0][IP_LAYER_SIZE-1 : 0],
+    input  [WORD_SIZE-1 : 0] B [OP_LAYER_SIZE-1 : 0]
     );
     parameter DEC_SLICE = WORD_SIZE - INT_SLICE;
 
-    reg [WORD_SIZE-1 : 0] res [LAYER_SIZE-1 : 0];
-    reg [2*WORD_SIZE-1 : 0] intrmd [LAYER_SIZE-1 : 0][LAYER_SIZE-1 : 0];
-    reg [2*WORD_SIZE-1 : 0] intrmdinv [LAYER_SIZE-1 : 0][LAYER_SIZE-1 : 0];
-    reg [WORD_SIZE-1 : 0] prod [LAYER_SIZE-1 : 0][LAYER_SIZE-1 : 0];
-    reg [WORD_SIZE-1 : 0] prodinv [LAYER_SIZE-1 : 0][LAYER_SIZE-1 : 0];
+    reg [WORD_SIZE-1 : 0] res [OP_LAYER_SIZE-1 : 0];
+    reg [2*WORD_SIZE-1 : 0] intrmd [OP_LAYER_SIZE-1 : 0][IP_LAYER_SIZE-1 : 0];
+    reg [2*WORD_SIZE-1 : 0] intrmdinv [OP_LAYER_SIZE-1 : 0][IP_LAYER_SIZE-1 : 0];
+    reg [WORD_SIZE-1 : 0] prod [OP_LAYER_SIZE-1 : 0][IP_LAYER_SIZE-1 : 0];
+    reg [WORD_SIZE-1 : 0] prodinv [IP_LAYER_SIZE-1 : 0][IP_LAYER_SIZE-1 : 0];
     
     integer i,j;
 	always begin
-        for(i = 0 ; i < LAYER_SIZE ; i = i + 1) begin
+        for(i = 0 ; i < OP_LAYER_SIZE ; i = i + 1) begin
             res[i] = 0;
-            for(j = 0 ; j < LAYER_SIZE ; j = j + 1) begin
-                intrmd[i][j] = X[i] * W[i][j];
+            for(j = 0 ; j < IP_LAYER_SIZE ; j = j + 1) begin
+                intrmd[i][j] = X[j] * W[i][j];
                 if(intrmd[i][j][WORD_SIZE-1]) begin
                     intrmdinv[i][j] = ~intrmd[i][j] + 1; // 2's compliment
                     prodinv[i][j] = intrmdinv[i][j][WORD_SIZE+INT_SLICE-1 : WORD_SIZE-DEC_SLICE];
