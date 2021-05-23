a73665b0ae8e4d328aff9a120690218e546327f3
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-05-23 02:36:33 +0200

    finish reading from filter

--- io_module/compressor/main.js
+++ io_module/compressor/main.js
@@ -3,12 +3,73 @@ let arr = [];
 let str = ``;
 let splittedResult;
 let finalResult = [];
-
+let filter_string = "";
 const IMG_CHOICE = 1;
 const FILTER_CHOICE = 2;
 
 function dec2bin(dec) {
   return Number(dec).toString(2);
+}
+
+function float2bin(x) {
+  var correctedFloat = "";
+  var intg = "";
+  var str = "";
+  var finalOut = 0.0;
+  var floatPart = 0.0;
+  let i = 0;
+  var powerSign = "";
+  var expon = 0;
+  var value = 0;
+  var shifted = 0.0;
+  var flag = false; // false => positve
+
+  if (x.includes("e")) {
+    x = x.split("e");
+    powerSign = x[1][0];
+    expon = parseInt(x[1].slice(1));
+    value = parseFloat(x[0]);
+
+    if (powerSign == "-") {
+      shifted = value * Math.pow(10, -expon);
+    } else {
+      shifted = value * Math.pow(10, expon);
+    }
+  } else {
+    shifted = x;
+  }
+
+  flag = shifted > 0 ? false : true;
+
+  str = shifted.toString();
+  str = str.split(".");
+
+  // less than 15
+  intg = Number(str[0]).toString(2);
+
+  intg = new Array(6 - intg.length).join("0") + intg;
+
+  floatPart = parseFloat("." + str[1]);
+
+  while (i < 11) {
+    floatPart = floatPart * 2;
+    tmp = parseInt(floatPart);
+    correctedFloat += tmp.toString();
+    floatPart -= tmp;
+    i++;
+  }
+  finalOut = intg + correctedFloat;
+
+  if (flag) {
+    var complement = "";
+    for (var j = 0; j < finalOut.length; j++) {
+      complement += finalOut[j] === "0" ? "1" : "0";
+    }
+    result = parseInt(complement, 2) + 1;
+    finalOut = result.toString(2);
+  }
+
+  return finalOut;
 }
 
 function mergeAndWirte(arr, fileName) {
@@ -18,7 +79,7 @@ function mergeAndWirte(arr, fileName) {
 
   const res = Array.from(
     { length: arr.length / 2 },
-    (_, i) => arr[2 * i] + " " + arr[2 * i + 1]
+    (_, i) => arr[2 * i] +  arr[2 * i + 1]
   );
   let text = res.join("\n");
   fs.writeFileSync(fileName, text, "utf8");
@@ -68,7 +129,7 @@ function readImg(imgPath, output) {
   });
 }
 
-function readFilters(argArr) {
+function readFilters(argArr,output) {
   [1, 2, 3].forEach((el) => {
     argArr.shift();
   });
@@ -84,14 +145,26 @@ function readFilters(argArr) {
     text = text.split(" ");
     compine_filters = compine_filters.concat(text);
   });
-  console.log(compine_filters);
+  compine_filters = compine_filters.filter(function (e) {
+    return e !== "";
+  });
+
+  compine_filters.map((el) => {
+    filter_string += float2bin(el);
+  });
+  filter_string = RLE(filter_string);
+
+  splittedResult = filter_string.split("/");
+  splittedResult.pop(); //remove space (last element)
+  finalFormat(splittedResult, output);
 }
 switch (parseInt(process.argv[2])) {
   case IMG_CHOICE:
     readImg(process.argv[3], process.argv[4]);
     break;
   case FILTER_CHOICE:
-    readFilters(process.argv);
+    var out = process.argv.pop();
+    readFilters(process.argv, out);
     break;
   default:
     break;
